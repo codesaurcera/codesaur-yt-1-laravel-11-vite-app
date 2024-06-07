c941b989ef094afc278fd86623da5bf6664ca6f4
Codesaur <codesaurcera@gmail.com>
2024-06-07 13:01:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Laravel 11 with Bootstrap 5 x jQuery 3 x Sass
+# Vite Development
+
+Laravel jQuery Starter App
+
+![App Image 1](/assets/images/yt-vid1-img-1.png)
---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+// jQuery
+import jQuery from 'jquery';
+window.$ = jQuery;
+
+// I changed the bootstrap bundle to bootstrap to use bootstrap popper or collapsible components.
+import 'bootstrap';
+
+
